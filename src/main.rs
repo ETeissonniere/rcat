c9690da48e2fce72d6814c9ee1cc0fe9da3c5678
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-07-24 11:23:37 +0200

    Use macros for CLI

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
+#[macro_use]
 extern crate clap;
-use clap::{Arg, App, SubCommand};
 
 fn main() {
-    let matches = App::new("rcat")
-        .version("1.0")
-        .author("Eliott Teissonniere <eliott.teissonniere.org>")
-        .about("Tiny netcat like program in rust")
-        .arg(Arg::with_name("port")
-            .short("p")
-            .long("port")
-            .default_value("4242")
-            .help("Specify port to use when listening or connecting")
-            .value_name("PORT")
-            .takes_value(true))
-        .arg(Arg::with_name("address")
-            .short("a")
-            .long("address")
-            .default_value("127.0.0.1")
-            .help("Specify IP address to use when listening or connecting")
-            .value_name("ADDR")
-            .takes_value(true))
-        .subcommand(SubCommand::with_name("listen")
-            .about("start listening server"))
-        .subcommand(SubCommand::with_name("connect")
-            .about("connect to server"))
-        .get_matches();
+    let matches = clap_app!(rcat => 
+        (version: "1.0")
+        (author: "Eliott Teissonniere <eliott.teissonniere.org>")
+        (about: "Tiny netcat like program in rust")
+        (@arg PORT: -p --port +takes_value "Port to connect to or listen on")
+        (@arg ADDR: -a --address +takes_value "Address to connect to or listen on")
+        (@subcommand listen => 
+            (about: "Listen for incoming connections on specified address and port")
+        )
+        (@subcommand connect =>
+            (about: "Connect to specified address and port")
+        )
+    ).get_matches();
+    
+    if let Some(_) = matches.subcommand_matches("listen") {
+        println!("Listening on {}:{}", matches.value_of("ADDR").unwrap_or("127.0.0.1"), matches.value_of("PORT").unwrap_or("4242"))
+    } else if let Some(_) = matches.subcommand_matches("connect") {
+        println!("Connecting to {}:{}", matches.value_of("ADDR").unwrap_or("127.0.0.1"), matches.value_of("PORT").unwrap_or("4242"))
+    } else {
+        println!("Unknown command")
+    }
 }
